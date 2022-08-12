2d8ba08f8a4e4619fec87cbf5ad7ba3123d89a7b
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2022-08-12 15:48:21 +1200

    added sendgrid, should build now

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,21 @@ import { React } from "react"
 import styled from '@emotion/styled';
 import Layout from "../components/layout";
 
+const Wrapper = styled.div`
+margin: auto;
+h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 70px;
+}
+`
+
 export default function Projects(){
     return(
         <Layout>
-            <h1>Projects</h1>
+            <Wrapper>
+                <h1>Projects</h1>
+            </Wrapper>
         </Layout>
     )
 }
